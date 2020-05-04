0fe0fe07fe1c918c3fcf55514260ef1f2b18d2d8
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-04 14:13:13 +1200

    Adding nrpe service

--- code/modules/nagios/manifests/config.pp
+++ code/modules/nagios/manifests/config.pp
@@ -128,4 +128,20 @@ class nagios::config
 		contact_groups => "admins",
 		mode => "0644",
 	}
+	
+	nagios_service { "nrpe":
+		service_description => "nrpe servers",
+		hostgroup_name => "remote-disks",
+		target => "/etc/nagios3/conf.d/ppt_services.cfg",
+		check_command => "check_nrpe_1arg!check_hd",
+		max_check_attempts => 3,
+		retry_check_interval => 1,
+		normal_check_interval => 5,
+		check_period => "24x7",
+		notification_interval => 30,
+		notification_period => "24x7",
+		notification_options => "w,u,c",
+		contact_groups => "admins",
+		mode => "0644",
+	}
 }
