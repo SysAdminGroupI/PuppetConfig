31cd680cd8f0a34b892e6afe343aa57430a88cb5
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-19 10:10:27 +1200

    adding more codes

--- code/modules/backup/backup.sh
+++ code/modules/backup/backup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 rsync -azP /etc/hostname i@10.25.137.160:~/backup/mgmt
 rsync -azP /etc/hosts i@10.25.137.160:~/backup/mgmt
+rsync -azP /etc/nagios3/htpasswd.users i@10.25.137.160:~/backup/mgmt/nagios3
+rsync -azP /etc/nagios3/nagios.cfg i@10.25.137.160:~/backup/mgmt/nagios3
+rsync -azP /etc/nagios-plugins/config/slack.cfg i@10.25.137.160:~/backup/mgmt/nagios-plugins/slack
+rsync -azP /usr/lib/nagios/plugins/nagios.pl i@10.25.137.160:~/backup/mgmt/nagios-plugins/slack
