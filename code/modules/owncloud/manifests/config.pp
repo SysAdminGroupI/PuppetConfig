378702e2e16f5f1b7b56a477ab9909c45cf5d219
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-26 12:12:25 +1200

    Adding ownloud release key puppet code

--- code/modules/owncloud/manifests/config.pp
+++ code/modules/owncloud/manifests/config.pp
@@ -1,4 +1,11 @@
 class owncloud::config {
+	exec { 'download owncloud realese key':
+		command => 'wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_19.10/Release.key -O Release.key',
+		user => 'root',
+		creates => '~/Release.key'
+		path => ['~/'],
+	}
+	
 	file { "/etc/apt/sources.list.d/owncloud.list":
 		ensure => present,
 		source_permissions => 'ignore',
