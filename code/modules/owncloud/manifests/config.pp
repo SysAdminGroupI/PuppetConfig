3029df911878f324234263aa11e0acf7ae0285cb
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-26 12:39:00 +1200

    Adding puppet code to add key

--- code/modules/owncloud/manifests/config.pp
+++ code/modules/owncloud/manifests/config.pp
@@ -1,8 +1,15 @@
 class owncloud::config {
-	exec { 'wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_19.10/Release.key -O Release.key | apt-key add - < Release.key':
+	exec { 'wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_19.10/Release.key -O Release.key':
 		user => 'root',
 		creates => '/home/student/Release.key',
 		path => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+	}
+	
+	exec { 'apt-key add - < Release.key':
+		user => 'root',
+		creates => '/home/student/Release.key',
+		path => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
+		require => Exec['wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_19.10/Release.key -O Release.key'],
 	}
 	
 	file { "/etc/apt/sources.list.d/owncloud.list":
