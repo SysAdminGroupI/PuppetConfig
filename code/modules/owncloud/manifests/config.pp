66a2e8880fdc7f68772a4d407742e10691e163f1
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-26 12:28:46 +1200

    Fixing syntax error

--- code/modules/owncloud/manifests/config.pp
+++ code/modules/owncloud/manifests/config.pp
@@ -2,7 +2,7 @@ class owncloud::config {
 	exec { 'wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_19.10/Release.key -O Release.key':
 		user => 'root',
 		creates => '/home/student/Release.key',
-		path => ['/home/student/'],
+		path => [ '/bin/','/sbin/','/usr/bin/','/usr/sbin/' ],
 	}
 	
 	file { "/etc/apt/sources.list.d/owncloud.list":
