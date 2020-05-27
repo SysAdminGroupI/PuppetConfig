c249a7690ab76e032efe4b016cca56a26b6215bb
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-28 08:02:04 +1200

    cleanup

--- code/modules/puppetconf/manifests/init.pp
+++ code/modules/puppetconf/manifests/init.pp
@@ -1,13 +1,13 @@
 class puppetconf {
- file { '/etc/puppet/puppet.conf':
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  source => 'puppet:///modules/puppetconf/puppet.conf'
- }
+    file { '/etc/puppet/puppet.conf':
+        owner => 'root',
+        group => 'root',
+        mode => '0644',
+        source => 'puppet:///modules/puppetconf/puppet.conf'
+    }
 
- service { 'puppet':
-  ensure => 'running',
-  enable => 'true'
- }
+    service { 'puppet':
+        ensure => 'running',
+        enable => 'true'
+    }
 }
