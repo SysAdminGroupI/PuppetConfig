79e8d5746fdc3a43a025e5bb2fed2cb3439abcfd
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-28 08:04:13 +1200

    cleanup

--- code/modules/userconf/manifests/init.pp
+++ code/modules/userconf/manifests/init.pp
@@ -1,36 +1,40 @@
 class userconf {
+	user { 'nobody':
+		name => 'nobody',
+		ensure => 'present',
+		gid => 'nogroup',
+		home => '/home/nonexistent',
+		shell => '/usr/sbin/nologin',
+		require => Group['nogroup']
+	}
 
-user { 'nobody':
-	name => 'nobody',
-	ensure => 'present',
-	gid => 'nogroup',
-	home => '/home/nonexistent',
-	shell => '/usr/sbin/nologin',
-	require => Group['nogroup']
-}
-group {'nogroup':
-	ensure => 'present'
-}
-user {'student':
-	name => 'student',
-	ensure => 'present',
-	gid => 'student',
-	home => '/home/student',
-	shell => '/bin/bash',
-	require => Group['student']
-}
-group {'student':
-	ensure => 'present'
-}
-user {'manager':
-	name => 'manager',
-	ensure => 'present',
-	gid => 'manager',
-	home => '/home/manager/',
-	shell => '/bin/bash',
-	require => Group['manager']
-}
-group {'manager':
-	ensure => 'present'
-}
+	group { 'nogroup':
+		ensure => 'present'
+	}
+
+	user { 'student':
+		name => 'student',
+		ensure => 'present',
+		gid => 'student',
+		home => '/home/student',
+		shell => '/bin/bash',
+		require => Group['student']
+	}
+
+	group { 'student':
+		ensure => 'present'
+	}
+
+	user { 'manager':
+		name => 'manager',
+		ensure => 'present',
+		gid => 'manager',
+		home => '/home/manager/',
+		shell => '/bin/bash',
+		require => Group['manager']
+	}
+
+	group { 'manager':
+		ensure => 'present'
+	}
 }
