d314c8e9387fcba3f266982284e508c4fb073d34
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-05-28 09:56:46 +1200

    adding check interval

--- code/modules/nagios/manifests/config.pp
+++ code/modules/nagios/manifests/config.pp
@@ -57,16 +57,16 @@ class nagios::config
 	
 	# Nagios Hosts
 
-	nagios_host { "test-i.foo.org.nz":
+	nagios_host { "db-i.foo.org.nz":
 		target => "/etc/nagios3/conf.d/ppt_hosts.cfg",
 		alias => "db",
 		check_period => "24x7",
+		check_interval => 1,
 		max_check_attempts => 3,
 		check_command => "check-host-alive",
-		check_interval => 1,
 		notification_period => "24x7",
 		notification_interval => 30,
-		notification_options => "d,w,u,r",
+		notification_options => "d,u,r",
 		contact_groups => "slackgroup",
 		mode => "0644",
 	}
@@ -75,6 +75,7 @@ class nagios::config
 		target => "/etc/nagios3/conf.d/ppt_hosts.cfg",
 		alias => "app",
 		check_period => "24x7",
+		check_interval => 1,
 		max_check_attempts => 3,
 		check_command => "check-host-alive",
 		notification_period => "24x7",
@@ -87,6 +88,7 @@ class nagios::config
 		target => "/etc/nagios3/conf.d/ppt_hosts.cfg",
 		alias => "back",
 		check_period => "24x7",
+		check_interval => 1,
 		max_check_attempts => 3,
 		check_command => "check-host-alive",
 		notification_period => "24x7",
