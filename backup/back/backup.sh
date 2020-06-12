759397883f3055f52d2f58a1803f0cb1ed300c48
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-06-12 14:47:17 +1200

    updating back-i backup script

--- backup/back/backup.sh
+++ backup/back/backup.sh
@@ -4,4 +4,8 @@ rsync -azP /etc/hosts i@10.25.137.160:~/backup/back/
 rsync -azP /etc/nagios/nrpe.cfg i@10.25.137.160:~/backup/back/nrpe/
 rsync -azP ~/prometheus-2.18.1.linux-amd64/ i@10.25.137.160:~/backup/back/prometheus/prometheus/
 rsync -azP ~/node_exporter-1.0.0-rc.0.linux-amd64/ i@10.25.137.160:~/backup/back/prometheus/node-exporter/
+mysqldump -u student -A --add-drop-database --add-locks > ~/db-backup.sql
+mysqldump -u student owncloud --add-locks > ~/owncloud-backup.sql
+rsync -azP ~/db-backup.sql i@10.25.137.160:~/backup/back/
+rsync -azP ~/owncloud-backup.sql i@10.25.137.160:~/backup/back/
 rsync -azP ~/backup.sh i@10.25.137.160:~/backup/back/
