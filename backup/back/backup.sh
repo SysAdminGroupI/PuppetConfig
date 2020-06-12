d739d09845cd575b6092b62a15166e4a4d5db31e
Carthur Pongpatimet <pongp1@student.op.ac.nz>
2020-06-12 14:56:29 +1200

    adding mysql table to backup

--- backup/back/backup.sh
+++ backup/back/backup.sh
@@ -6,6 +6,8 @@ rsync -azP ~/prometheus-2.18.1.linux-amd64/ i@10.25.137.160:~/backup/back/promet
 rsync -azP ~/node_exporter-1.0.0-rc.0.linux-amd64/ i@10.25.137.160:~/backup/back/prometheus/node-exporter/
 mysqldump -u student -A --add-drop-database --add-locks > ~/db-backup.sql
 mysqldump -u student owncloud --add-locks > ~/owncloud-backup.sql
+mysqldump -u student mysql --add-locks > ~/mysql-backup.sql
 rsync -azP ~/db-backup.sql i@10.25.137.160:~/backup/back/
 rsync -azP ~/owncloud-backup.sql i@10.25.137.160:~/backup/back/
+rsync -azP ~/mysql-backup.sql i@10.25.137.160:~/backup/back/
 rsync -azP ~/backup.sh i@10.25.137.160:~/backup/back/
